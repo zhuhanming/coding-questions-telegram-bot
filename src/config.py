b3ad698035ae97eb269017fa02bab8cd3ca3447a
Hanming Zhu <hanming@huawen.com>
2021-04-23 18:36:39 +0800

    chore: update config with correct db values

--- src/config.py
+++ src/config.py
@@ -5,14 +5,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_ENV = getenv("BOT_ENV")
-DATABASE_URL = getenv("DATABASE_URL", "sqlite+pysqlite:///:memory:")
+DEFAULT_DATABASE_URL = ""
+if BOT_ENV == "DEVELOPMENT":
+    DEFAULT_DATABASE_URL = "postgresql://coding_questions_bot:coding_questions_bot@localhost/coding_questions_bot"
+elif BOT_ENV == "TEST":
+    DEFAULT_DATABASE_URL = "postgresql://coding_questions_bot:coding_questions_bot@localhost/coding_questions_bot_test"
+DATABASE_URL = getenv("DATABASE_URL", DEFAULT_DATABASE_URL)
 
 APP_CONFIG = {
     "DATABASE_URL": DATABASE_URL,
     "BOT_ACCESS_TOKEN": getenv("BOT_ACCESS_TOKEN"),
     "DEVELOPER_ID": getenv("DEVELOPER_ID"),
     "WEEKLY_TARGET": 7,
-    "TRACEBACK_LENGTH": 300,
+    "TRACEBACK_LENGTH": 600,
     "PLATFORM_KEY": "PLATFORM",
     "QUESTION_NAME_KEY": "QUESTION_NAME",
 }
