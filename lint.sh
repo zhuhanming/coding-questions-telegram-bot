89b2c57a9cd81cecd02d29a480168b87b2c181a9
Hanming Zhu <hanming@huawen.com>
2021-04-24 13:31:17 +0800

    fix: fix lint.sh

--- lint.sh
+++ lint.sh
@@ -2,10 +2,10 @@
 set -e
 
 poetry run black --check .
-poetry run isort --recursive --diff --check-only
+poetry run isort --diff --check-only .
 poetry run flake8 --select=F
 
 mv requirements.txt requirements2.txt
-poetry export --without-hashes -f requirements.txt
+poetry export --without-hashes -f requirements.txt  > requirements.txt
 diff requirements.txt requirements2.txt || (mv requirements2.txt requirements.txt; false)
 mv requirements2.txt requirements.txt
