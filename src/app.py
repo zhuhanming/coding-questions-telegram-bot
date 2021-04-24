aed8e8712cf5c0b21da9cc7d5c4bfa3db76847fa
Hanming Zhu <hanming@huawen.com>
2021-04-24 10:05:33 +0800

    feat: add new chat member handler to app

--- src/app.py
+++ src/app.py
@@ -1,8 +1,14 @@
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 
+from src.add_handlers import add_conv_handler
 from src.config import APP_CONFIG
-from src.handle_add import add_conv_handler
-from src.handle_general import error_handler, start, unknown_message
+from src.general_handlers import (
+    error_handler,
+    new_chat_member_handler,
+    start,
+    unknown_message,
+)
+from src.stats_handlers import week
 
 
 def main() -> None:
@@ -10,9 +16,13 @@ def main() -> None:
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("week", week))
     dispatcher.add_handler(add_conv_handler)
     dispatcher.add_handler(
         MessageHandler(Filters.text & ~Filters.command, unknown_message)
+    )
+    dispatcher.add_handler(
+        MessageHandler(Filters.status_update.new_chat_members, new_chat_member_handler)
     )
     dispatcher.add_error_handler(error_handler)
 
