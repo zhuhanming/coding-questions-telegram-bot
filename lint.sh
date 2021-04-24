b2fa24c11b38012de9f5695bd69ae09fd50369d3
Hanming Zhu <hanming@huawen.com>
2021-04-24 14:07:46 +0800

    fix: fix lint script and action

--- lint.sh
+++ lint.sh
@@ -9,5 +9,3 @@ mv requirements.txt requirements2.txt
 poetry export --without-hashes -f requirements.txt  > requirements.txt
 diff requirements.txt requirements2.txt || (mv requirements2.txt requirements.txt; false)
 mv requirements2.txt requirements.txt
-
-exit 0
