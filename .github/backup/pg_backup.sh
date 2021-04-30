c04f3414ffea50d44d2e43e14eb328e9693edc12
Hanming Zhu <hanming@huawen.com>
2021-05-01 01:51:01 +0800

    fix: keep scope of env variable to pg_dump only

--- .github/backup/pg_backup.sh
+++ .github/backup/pg_backup.sh
@@ -59,7 +59,7 @@ function perform_backups()
   do
     echo "Uncompressed backup of $DATABASE"
 
-    if ! PGPASSWORD="$PASSWORD" pg_dump -O -h "$HOSTNAME" -U "$USERNAME" "$DATABASE" > $BACKUP_DIR"$BACKUP_NAME".sql.in_progress; then
+    if ! (export PGPASSWORD="$PASSWORD"; pg_dump -O -h "$HOSTNAME" -U "$USERNAME" "$DATABASE" > $BACKUP_DIR"$BACKUP_NAME".sql.in_progress); then
       echo "Failed to produce uncompressed backup of database $DATABASE" 1>&2
     else
       mv $BACKUP_DIR"$BACKUP_NAME".sql.in_progress $BACKUP_DIR"$BACKUP_NAME".sql
