ddcf879bdcc1f17aa6aef5a5a49fae39ae48147b
Hanming Zhu <hanming@huawen.com>
2021-04-30 22:19:34 +0800

    feat: add backup script for pg database

---
+++ .github/backup/pg_backup.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+###########################
+####### LOAD CONFIG #######
+###########################
+
+if [ -z $CONFIG_FILE_PATH ] ; then
+  CONFIG_FILE_PATH="$1/.github/backup/pg_backup.config"
+fi
+
+if [ ! -r ${CONFIG_FILE_PATH} ] ; then
+  echo "Could not load config file from ${CONFIG_FILE_PATH}" 1>&2
+  exit 1
+fi
+
+source "${CONFIG_FILE_PATH}"
+
+###########################
+### INITIALISE DEFAULTS ###
+###########################
+
+if [ ! $HOSTNAME ]; then
+  HOSTNAME="localhost"
+fi;
+
+if [ ! $USERNAME ]; then
+  USERNAME="postgres"
+fi;
+
+BACKUP_DIR="$1/backups/"
+
+###########################
+#### START THE BACKUPS ####
+###########################
+
+function perform_backups()
+{
+  BACKUP_NAME="`date +\%Y-\%m-\%d`-deploy"
+
+  echo "Making backup directory in $BACKUP_DIR"
+
+  if ! mkdir -p $BACKUP_DIR; then
+    echo "Cannot create backup directory in $BACKUP_DIR." 1>&2
+    exit 1;
+  fi;
+
+  ###########################
+  ###### FULL BACKUPS #######
+  ###########################
+
+  echo -e "\n\nPerforming full backups"
+  echo -e "--------------------------------------------\n"
+
+  for DATABASE in ${DATABASE_WHITELIST//,/ }
+  do
+    echo "Uncompressed backup of $DATABASE"
+
+    if ! pg_dump "$DATABASE" > $BACKUP_DIR"$BACKUP_NAME".sql.in_progress; then
+      echo "Failed to produce uncompressed backup of database $DATABASE" 1>&2
+    else
+      mv $BACKUP_DIR"$BACKUP_NAME".sql.in_progress $BACKUP_DIR"$BACKUP_NAME".sql
+    fi
+  done
+
+  echo -e "\nAll database backups complete!"
+}
+
+# DEPLOYMENT BACKUPS
+
+find $BACKUP_DIR -maxdepth 1 -name "*-deploy.*" -exec rm -rf '{}' ';'
+perform_backups
+exit 0;
