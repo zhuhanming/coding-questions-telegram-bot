c770d5dab3893c6dce9f142c6b726eb3763f8d2b
Hanming Zhu <hanming@huawen.com>
2022-08-13 21:19:48 +0800

    feat: add time utils

---
+++ src/utils/time.py
@@ -0,0 +1,24 @@
+from datetime import datetime, timedelta
+
+import pytz
+
+TIMEZONE = tz = pytz.timezone("Asia/Singapore")
+
+
+def get_start_of_week() -> datetime:
+    now = datetime.now(tz=TIMEZONE)
+    return (now - timedelta(days=now.weekday())).replace(
+        hour=0, minute=0, second=0, microsecond=0
+    )
+
+
+def get_start_of_last_week() -> datetime:
+    now = datetime.now(tz=TIMEZONE)
+    return (now - timedelta(days=now.weekday() + 7)).replace(
+        hour=0, minute=0, second=0, microsecond=0
+    )
+
+
+def get_start_of_month() -> datetime:
+    now = datetime.now(tz=TIMEZONE)
+    return now.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
