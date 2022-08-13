c225be01a779b57425fbcd7b2d47989463a4538f
Hanming Zhu <hanming@huawen.com>
2022-08-13 21:19:12 +0800

    feat: add SummaryType enum

---
+++ src/utils/enums.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class SummaryType(Enum):
+    WEEKLY = "week"
+    MONTHLY = "month"
+    ALL = "all time"
+    ALL_UNIQUE = "all time (unique)"
+
+    def format(self, is_last_week: bool = False) -> str:
+        if self.name in ["WEEKLY", "MONTHLY"]:
+            return ("last " if is_last_week else "this ") + self.value
+        return self.value
