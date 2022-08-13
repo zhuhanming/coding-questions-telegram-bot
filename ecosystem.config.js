b4c93b06d6d317becd4b36060d6008e8602847c1
Hanming Zhu <hanming@huawen.com>
2022-08-14 02:05:29 +0800

    chore: update files to use main.py instead of app.py

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "coding-questions-bot",
-      script: "src/app.py",
+      script: "src/main.py",
       autorestart: false,
       interpreter: "poetry",
       interpreter_args: "run python",
--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ "$MANUAL_PATH" = "1" ]; then
-  env PYTHONPATH=. $1 run python src/app.py
+  env PYTHONPATH=. $1 run python src/main.py
 else
-  env PYTHONPATH=. poetry run python src/app.py
+  env PYTHONPATH=. poetry run python src/main.py
 fi
