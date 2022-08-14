c78d1c24a9130b36ce261ef18502caf20e3ddd97
Hanming Zhu <hanming@huawen.com>
2022-08-14 23:35:20 +0800

    feat: add remaining stat methods

--- src/handlers/view_stats.py
+++ src/handlers/view_stats.py
@@ -1,14 +1,33 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import Application, CallbackQueryHandler, ContextTypes
 
-from ..utils import MONTH_ALL_SUMMARY_STRFTIME_FORMAT, SummaryType, format_platform_name
+from ..utils import (
+    MONTH_ALL_SUMMARY_STRFTIME_FORMAT,
+    WEEK_SUMMARY_STRFTIME_FORMAT,
+    SummaryType,
+    difficulty_to_int,
+    format_platform_name,
+    platform_to_int,
+)
 from .base import BaseHandler
 
 
 class ViewStatsHandler(BaseHandler):
     def bind(self, app: Application) -> None:
         app.add_handler(CallbackQueryHandler(self.view_stats, "view_stats"))
+        app.add_handler(
+            CallbackQueryHandler(self.this_week_questions, "this_week_questions")
+        )
+        app.add_handler(
+            CallbackQueryHandler(self.last_week_questions, "last_week_questions")
+        )
+        app.add_handler(
+            CallbackQueryHandler(self.this_month_questions, "this_month_questions")
+        )
         app.add_handler(CallbackQueryHandler(self.all_questions, "all_questions"))
+        app.add_handler(
+            CallbackQueryHandler(self.all_unique_questions, "all_unique_questions")
+        )
 
     async def view_stats(
         self, update: Update, context: ContextTypes.DEFAULT_TYPE
@@ -21,8 +40,39 @@ class ViewStatsHandler(BaseHandler):
             self.__get_stats_keyboard(),
         )
 
+    async def this_week_questions(
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ) -> None:
+        await self.__helper(update, context, SummaryType.WEEKLY)
+
+    async def last_week_questions(
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ) -> None:
+        await self.__helper(update, context, SummaryType.WEEKLY, True)
+
+    async def this_month_questions(
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ) -> None:
+        await self.__helper(update, context, SummaryType.MONTHLY)
+
     async def all_questions(
         self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ) -> None:
+        await self.__helper(update, context, SummaryType.ALL)
+
+    async def all_unique_questions(
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ) -> None:
+        await self.__helper(update, context, SummaryType.ALL_UNIQUE)
+
+    # TODO: Handle weekly targets of chats
+    # TODO: Handle opt-out timings, i.e. time-travelling where necessary
+    async def __helper(
+        self,
+        update: Update,
+        context: ContextTypes.DEFAULT_TYPE,
+        summary_type: SummaryType,
+        is_last_week: bool = False,
     ) -> None:
         query = await self._get_and_answer_query(update)
         user = self._get_user(update)
@@ -30,19 +80,35 @@ class ViewStatsHandler(BaseHandler):
             full_name=user.full_name, telegram_id=str(user.id)
         )
         records = self.service.question_record.get_records_by_user(
-            user_id=user_dict["id"], summary_type=SummaryType.ALL
+            user_id=user_dict["id"],
+            summary_type=summary_type,
+            is_last_week=is_last_week,
         )
         if not records:
             await self._edit_query_text(
                 context,
                 query,
-                "You have not completed any questions!",
+                "You did not complete any questions!",
                 InlineKeyboardMarkup(self.__get_back_keyboard()),
             )
+            return
 
-        header_text = "Questions you have completed all time"
+        strftime_format = MONTH_ALL_SUMMARY_STRFTIME_FORMAT
+        if summary_type == SummaryType.WEEKLY:
+            strftime_format = WEEK_SUMMARY_STRFTIME_FORMAT
+        if summary_type == SummaryType.ALL_UNIQUE:
+            records.sort(
+                key=lambda x: (
+                    difficulty_to_int(x["difficulty"]),
+                    platform_to_int(x["platform"]),
+                )
+            )
+
+        header_text = (
+            f"Questions you have completed {summary_type.format(is_last_week)}"
+        )
         items = [
-            f'{record["question_name"]} [{record["difficulty"].title()}] [{format_platform_name(record["platform"])}] ({record["created_at"].strftime(MONTH_ALL_SUMMARY_STRFTIME_FORMAT)})'
+            f'{record["question_name"]} [{record["difficulty"].title()}] [{format_platform_name(record["platform"])}]{"" if summary_type == SummaryType.ALL_UNIQUE else " (" + record["created_at"].strftime(strftime_format) + ")"}'
             for record in records
         ]
         pagination = self.helper.pagination.create_and_set_data(
