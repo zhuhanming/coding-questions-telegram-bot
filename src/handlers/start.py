2f4613cf967db7e92e103ab3f0d280e1d58254b2
Hanming Zhu <hanming@huawen.com>
2022-08-14 18:02:57 +0800

    feat: allow return to start via callback query

--- src/handlers/start.py
+++ src/handlers/start.py
@@ -1,5 +1,5 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import Application, CallbackQueryHandler, CommandHandler, ContextTypes
 
 from .base import BaseHandler
 
@@ -7,6 +7,7 @@ from .base import BaseHandler
 class StartHandler(BaseHandler):
     def bind(self, app: Application) -> None:
         app.add_handler(CommandHandler("start", self.start))
+        app.add_handler(CallbackQueryHandler(self.start_query, "start"))
 
     async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         user = self._get_user(update)
@@ -25,6 +26,19 @@ class StartHandler(BaseHandler):
         await self._reply_text(
             context,
             message,
+            f"Hello {user.full_name}! What do you want to do today?",
+            self.__get_private_chat_keyboard(),
+        )
+
+    async def start_query(
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ) -> None:
+        query = await self._get_and_answer_query(update)
+        user = self._get_user(update)
+        self.helper.logger.info(f"User started: {user.full_name}")
+        await self._edit_query_text(
+            context,
+            query,
             f"Hello {user.full_name}! What do you want to do today?",
             self.__get_private_chat_keyboard(),
         )
