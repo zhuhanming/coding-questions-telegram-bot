b633b576fe834b7838b567b5cba85f461305c1a1
Hanming Zhu <hanming@huawen.com>
2022-08-14 22:55:19 +0800

    feat: remove unused enum method

--- src/utils/enums.py
+++ src/utils/enums.py
@@ -6,8 +6,3 @@ class SummaryType(Enum):
     MONTHLY = "month"
     ALL = "all time"
     ALL_UNIQUE = "all time (unique)"
-
-    def format(self, is_last_week: bool = False) -> str:
-        if self.name in ["WEEKLY", "MONTHLY"]:
-            return ("last " if is_last_week else "this ") + self.value
-        return self.value
