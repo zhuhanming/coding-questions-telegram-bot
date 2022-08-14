7c2c39b1996f675cda05f1c62b6c0eb6b1b875a7
Hanming Zhu <hanming@huawen.com>
2022-08-14 16:51:30 +0800

    feat: refactor code to make add_question handler cleaner

--- src/handlers/add_question.py
+++ src/handlers/add_question.py
@@ -11,12 +11,11 @@ from telegram.ext import (
 )
 
 from ..helpers import HACKERRANK_REGEX, LEETCODE_REGEX
-from ..utils import InvalidUserDataException, unwrap
 from .base import BaseHandler
 
 FETCH, MANUAL_NAME, MANUAL_DIFFICULTY_PRE, MANUAL_DIFFICULTY, THANKS = range(5)
 
-
+# TODO: Add comments to code
 class AddQuestionHandler(BaseHandler):
     def bind(self, app: Application) -> None:
         app.add_handler(
@@ -25,7 +24,7 @@ class AddQuestionHandler(BaseHandler):
                 states={
                     FETCH: [
                         MessageHandler(
-                            filters.TEXT & ~filters.COMMAND, self.try_fetch_details
+                            filters.TEXT & ~filters.COMMAND, self.try_fetch_question
                         )
                     ],
                     MANUAL_NAME: [
@@ -47,155 +46,159 @@ class AddQuestionHandler(BaseHandler):
             )
         )
 
-    async def add_question(self, update: Update, _: ContextTypes.DEFAULT_TYPE) -> int:
-        query = unwrap(update.callback_query)
-        user = unwrap(update.effective_user)
-        await query.answer()
-
-        await query.edit_message_text(
-            f"Hi {user.full_name}! Glad to see that you've been working hard!\n"
+    async def add_question(
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ) -> int:
+        query = await self._get_and_answer_query(update)
+        await self._edit_query_text(
+            context,
+            query,
+            f"Hi {self._get_user(update).full_name}! Glad to see that you've been working hard!\n"
             "Can you send me the URL of the question that you've attempted?",
-            reply_markup=InlineKeyboardMarkup(self.__get_cancel_keyboard()),
+            self.__get_cancel_keyboard(),
         )
         return FETCH
 
-    async def try_fetch_details(
+    async def try_fetch_question(
         self, update: Update, context: ContextTypes.DEFAULT_TYPE
     ) -> int:
-        update.message = unwrap(update.message)
-        update.message.text = unwrap(update.message.text)
-        if context.user_data is None:
-            raise InvalidUserDataException()
+        await self._clear_previous_message_reply_markup(update, context)
+        original_message = self._get_message(update)
 
-        url = update.message.text.lower()
+        url = self._get_message_text(update).lower()
         platform = "other"
         if bool(match(LEETCODE_REGEX, url)):
             platform = "leetcode"
         elif bool(match(HACKERRANK_REGEX, url)):
             platform = "hackerrank"
-        context.user_data["PLATFORM"] = platform
+        self._set_context_value(context, "PLATFORM", platform)
 
         if platform == "other":
-            await update.message.reply_text(
-                "I was unable to fetch the question details from your URL.\n"
+            await self._reply_text(
+                context,
+                original_message,
+                "I was unable to fetch the question details from your URL.\n\n"
                 "Do you mind letting me know what the name of the question you attempted was?\n",
-                reply_markup=InlineKeyboardMarkup(self.__get_cancel_keyboard()),
+                self.__get_cancel_keyboard(),
             )
             return MANUAL_NAME
 
         is_leetcode = platform == "leetcode"
-        message = await update.message.reply_text(
-            "This part may take a while to load.\n" "Do be patient with me!"
+        message = await self._reply_text(
+            context,
+            original_message,
+            "This part may take a while to load.\n" "Do be patient with me!",
         )
         question_info = self.helper.question.get_question_info(
             url=url, is_leetcode=is_leetcode
         )
-        context.user_data["QUESTION_NAME"] = question_info.name
-        context.user_data["QUESTION_DIFFICULTY"] = (
+        self._set_context_value(context, "QUESTION_NAME", question_info.name)
+        self._set_context_value(
+            context,
+            "QUESTION_DIFFICULTY",
             question_info.difficulty.lower()
             if question_info.difficulty is not None
-            else None
+            else None,
         )
 
         if question_info.name is None:
-            await message.edit_text(
-                "It seems like either the URL is invalid or you sent me a premium question!\n"
+            await self._edit_message_text(
+                context,
+                message,
+                "It seems like either the URL is invalid or you sent me a premium question!\n\n"
                 "Please enter the name of the question manually.",
-                reply_markup=InlineKeyboardMarkup(self.__get_cancel_keyboard()),
+                self.__get_cancel_keyboard(),
             )
             return MANUAL_NAME
 
         if question_info.difficulty is None:
-            await message.edit_text(
-                "I tried my best to get the question title either from the website or from the URL you gave!\n"
+            await self._edit_message_text(
+                context,
+                message,
+                "I tried my best to get the question title either from the website or from the URL you gave!\n\n"
                 f"Is your question title: {question_info.name}?",
-                reply_markup=InlineKeyboardMarkup(self.__get_confirmation_keyboard()),
+                self.__get_confirmation_keyboard(),
             )
             return MANUAL_DIFFICULTY_PRE
 
-        assert question_info.name is not None
-        assert question_info.difficulty is not None
-
-        await message.edit_text(
-            "I managed to find the question info from the website directly!\n"
+        await self._edit_message_text(
+            context,
+            message,
+            "I managed to find the question info from the website directly!\n\n"
             f"Is your question: {question_info.name} [{question_info.difficulty.title()}]?",
-            reply_markup=InlineKeyboardMarkup(self.__get_confirmation_keyboard()),
+            self.__get_confirmation_keyboard(),
         )
         return THANKS
 
     async def manual_name(
         self, update: Update, context: ContextTypes.DEFAULT_TYPE
     ) -> int:
-        update.message = unwrap(update.message)
-        update.message.text = unwrap(update.message.text)
-        if context.user_data is None:
-            raise InvalidUserDataException()
-
-        question_name = update.message.text
-        context.user_data["QUESTION_NAME"] = question_name
-
-        await update.message.reply_text(
+        await self._clear_previous_message_reply_markup(update, context)
+        self._set_context_value(
+            context, "QUESTION_NAME", self._get_message_text(update)
+        )
+        await self._reply_text(
+            context,
+            self._get_message(update),
             "What is the difficulty of your question?",
-            reply_markup=InlineKeyboardMarkup(self.__get_difficulty_keyboard()),
+            self.__get_difficulty_keyboard(),
         )
         return MANUAL_DIFFICULTY
 
     async def manual_difficulty_pre(
-        self, update: Update, _: ContextTypes.DEFAULT_TYPE
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
     ) -> int:
-        query = unwrap(update.callback_query)
-        await query.answer()
-
+        query = await self._get_and_answer_query(update)
         if query.data == "no":
-            await query.edit_message_text(
-                "Sorry that I got your question title wrong!\n"
-                "Do you mind sending the name of the question here?"
+            await self._edit_query_text(
+                context,
+                query,
+                "Sorry that I got your question title wrong!\n\n"
+                "Do you mind sending the name of the question here?",
+                self.__get_cancel_keyboard(),
             )
             return MANUAL_NAME
 
-        await query.edit_message_text(
+        await self._edit_query_text(
+            context,
+            query,
             "Awesome! What is the difficulty of the question?",
-            reply_markup=InlineKeyboardMarkup(self.__get_difficulty_keyboard()),
+            self.__get_difficulty_keyboard(),
         )
         return MANUAL_DIFFICULTY
 
     async def manual_difficulty(
         self, update: Update, context: ContextTypes.DEFAULT_TYPE
     ) -> int:
-        query = unwrap(update.callback_query)
-        if context.user_data is None:
-            raise InvalidUserDataException()
-
-        question_difficulty = query.data
-        context.user_data["QUESTION_DIFFICULTY"] = question_difficulty
-        question_name = context.user_data["QUESTION_NAME"]
-
-        assert question_name is not None
-        assert question_difficulty is not None
-
-        await query.edit_message_text(
+        query = await self._get_and_answer_query(update)
+        question_difficulty = str(query.data)
+        self._set_context_value(context, "QUESTION_DIFFICULTY", question_difficulty)
+        question_name = self._get_context_value(context, "QUESTION_NAME")
+        await self._edit_query_text(
+            context,
+            query,
             f"Just to confirm, is your question: {question_name} [{question_difficulty.title()}]?",
-            reply_markup=InlineKeyboardMarkup(self.__get_confirmation_keyboard()),
+            self.__get_confirmation_keyboard(),
         )
         return THANKS
 
     async def thanks(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-        query = unwrap(update.callback_query)
-        user = unwrap(update.effective_user)
-        if context.user_data is None:
-            raise InvalidUserDataException()
-
+        query = await self._get_and_answer_query(update)
         if query.data == "no":
-            await query.edit_message_text(
+            await self._edit_query_text(
+                context,
+                query,
                 "Sorry that I got your question details wrong!\n"
-                "Do you mind sending the name of the question here?"
+                "Do you mind sending the name of the question here?",
+                self.__get_cancel_keyboard(),
             )
             return MANUAL_NAME
 
-        platform = context.user_data.get("PLATFORM")
-        question_name = context.user_data.get("QUESTION_NAME")
-        difficulty = context.user_data.get("QUESTION_DIFFICULTY")
+        platform = self._get_context_value(context, "PLATFORM")
+        question_name = self._get_context_value(context, "QUESTION_NAME")
+        difficulty = self._get_context_value(context, "QUESTION_DIFFICULTY")
 
+        user = self._get_user(update)
         # Persist data to database
         user_dict = self.service.user.create_if_not_exists(
             full_name=user.full_name, telegram_id=str(user.id)
@@ -215,55 +218,65 @@ class AddQuestionHandler(BaseHandler):
             difficulty,
         )
 
-        context.user_data.clear()
-        await query.edit_message_text(
-            "Awesome! Good job with the question!\n" "How else can I help you?",
-            reply_markup=InlineKeyboardMarkup(self.__get_final_keyboard()),
+        # TODO: Show some meaningful summary of data at this point
+        self._clear_context_values(context)
+        await self._edit_query_text(
+            context,
+            query,
+            "Awesome! Good job with the question!\n\n" "How else can I help you?",
+            self.__get_final_keyboard(),
         )
         return ConversationHandler.END
 
     async def cancel(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-        query = unwrap(update.callback_query)
-        if context.user_data is None:
-            raise InvalidUserDataException()
-
-        await query.edit_message_text(
-            "No worries! Feel free to add a question when you're ready!",
-            reply_markup=InlineKeyboardMarkup(self.__get_final_keyboard()),
+        query = await self._get_and_answer_query(update)
+        await self._edit_query_text(
+            context,
+            query,
+            "No worries! Feel free to add a question when you're ready!\n\n"
+            "How else can I help you?",
+            self.__get_final_keyboard(),
         )
-
-        context.user_data.clear()
+        self._clear_context_values(context)
         return ConversationHandler.END
 
-    def __get_cancel_keyboard(self) -> list[list[InlineKeyboardButton]]:
-        return [[InlineKeyboardButton(text="Cancel", callback_data="cancel")]]
+    def __get_cancel_keyboard(self) -> InlineKeyboardMarkup:
+        return InlineKeyboardMarkup(
+            [[InlineKeyboardButton(text="Cancel", callback_data="cancel")]]
+        )
 
-    def __get_confirmation_keyboard(self) -> list[list[InlineKeyboardButton]]:
-        return [
+    def __get_confirmation_keyboard(self) -> InlineKeyboardMarkup:
+        return InlineKeyboardMarkup(
             [
-                InlineKeyboardButton(text="Yes", callback_data="yes"),
-                InlineKeyboardButton(text="No", callback_data="no"),
+                [
+                    InlineKeyboardButton(text="Yes", callback_data="yes"),
+                    InlineKeyboardButton(text="No", callback_data="no"),
+                ]
             ]
-        ]
+        )
 
-    def __get_difficulty_keyboard(self) -> list[list[InlineKeyboardButton]]:
-        return [
-            [InlineKeyboardButton(text="Easy", callback_data="easy")],
-            [InlineKeyboardButton(text="Medium", callback_data="medium")],
-            [InlineKeyboardButton(text="Hard", callback_data="hard")],
-        ]
+    def __get_difficulty_keyboard(self) -> InlineKeyboardMarkup:
+        return InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton(text="Easy", callback_data="easy")],
+                [InlineKeyboardButton(text="Medium", callback_data="medium")],
+                [InlineKeyboardButton(text="Hard", callback_data="hard")],
+            ]
+        )
 
-    def __get_final_keyboard(self) -> list[list[InlineKeyboardButton]]:
-        return [
+    def __get_final_keyboard(self) -> InlineKeyboardMarkup:
+        return InlineKeyboardMarkup(
             [
-                InlineKeyboardButton(
-                    text="Add another question", callback_data="add_question"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    text="Complete an interview", callback_data="complete_interview"
-                )
-            ],
-            [InlineKeyboardButton(text="View stats", callback_data="view_stats")],
-        ]
+                [
+                    InlineKeyboardButton(
+                        text="Add another question", callback_data="add_question"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        text="Complete an interview", callback_data="complete_interview"
+                    )
+                ],
+                [InlineKeyboardButton(text="View stats", callback_data="view_stats")],
+            ]
+        )
--- src/handlers/base.py
+++ src/handlers/base.py
@@ -1,10 +1,14 @@
 from abc import ABC, abstractmethod
+from typing import Any, cast
 
-from telegram.ext import Application
+from telegram import CallbackQuery, Chat, InlineKeyboardMarkup, Message, Update, User
+from telegram.error import BadRequest
+from telegram.ext import Application, ContextTypes
 
 from ..config import Config
 from ..helpers import AppHelper
 from ..services import AppService
+from ..utils import InvalidUserDataException, unwrap
 
 
 class BaseHandler(ABC):
@@ -16,3 +20,106 @@ class BaseHandler(ABC):
     @abstractmethod
     def bind(self, app: Application) -> None:
         pass
+
+    # Helper methods for Telegram's Context object
+    def _get_context_value(self, context: ContextTypes.DEFAULT_TYPE, key: str) -> Any:
+        if context.user_data is None:
+            raise InvalidUserDataException()
+        return context.user_data[key]
+
+    def _set_context_value(
+        self, context: ContextTypes.DEFAULT_TYPE, key: str, value: Any
+    ) -> None:
+        if context.user_data is None:
+            raise InvalidUserDataException()
+        context.user_data[key] = value
+
+    def _clear_context_values(self, context: ContextTypes.DEFAULT_TYPE) -> None:
+        if context.user_data is None:
+            raise InvalidUserDataException()
+        context.user_data.clear()
+
+    # Helper methods for caching the previous message ID
+    def _get_previous_message_id(
+        self, context: ContextTypes.DEFAULT_TYPE
+    ) -> int | None:
+        return cast(int | None, self._get_context_value(context, "PREVIOUS_MESSAGE_ID"))
+
+    def _set_previous_message_id(
+        self, context: ContextTypes.DEFAULT_TYPE, message: Message | bool
+    ) -> None:
+        if type(message) is not bool:
+            self._set_context_value(context, "PREVIOUS_MESSAGE_ID", message.id)
+
+    # Helper get methods for Telegram's Update object
+    async def _get_and_answer_query(self, update: Update) -> CallbackQuery:
+        query = unwrap(update.callback_query)
+        await query.answer()
+        return query
+
+    def _get_user(self, update: Update) -> User:
+        return unwrap(update.effective_user)
+
+    def _get_message(self, update: Update) -> Message:
+        return unwrap(update.effective_message)
+
+    def _get_message_text(self, update: Update) -> str:
+        return unwrap(self._get_message(update).text)
+
+    def _get_chat(self, update: Update) -> Chat:
+        return unwrap(self._get_message(update).chat)
+
+    async def _clear_previous_message_reply_markup(
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ) -> None:
+        try:
+            await context.bot.edit_message_reply_markup(
+                self._get_chat(update).id,
+                self._get_previous_message_id(context),
+                reply_markup=None,
+            )
+        except BadRequest:
+            self.helper.logger.error("Tried to clear an already cleared reply markup")
+
+    # Helper methods for sending and editing messages
+    async def _reply_text(
+        self,
+        context: ContextTypes.DEFAULT_TYPE,
+        message: Message,
+        reply_text: str,
+        reply_markup: InlineKeyboardMarkup | None = None,
+    ) -> Message:
+        new_message = await message.reply_text(
+            reply_text,
+            reply_markup=reply_markup,
+        )
+        self._set_previous_message_id(context, new_message)
+        return cast(Message, new_message)
+
+    async def _edit_query_text(
+        self,
+        context: ContextTypes.DEFAULT_TYPE,
+        query: CallbackQuery,
+        edit_text: str,
+        reply_markup: InlineKeyboardMarkup | None = None,
+    ) -> Message:
+        new_message = await query.edit_message_text(
+            edit_text,
+            reply_markup=reply_markup,
+        )
+        self._set_previous_message_id(context, new_message)
+        return cast(Message, new_message)
+
+    async def _edit_message_text(
+        self,
+        context: ContextTypes.DEFAULT_TYPE,
+        message: Message,
+        edit_text: str,
+        reply_markup: InlineKeyboardMarkup | None = None,
+    ) -> Message:
+        new_message = await message.edit_text(
+            edit_text,
+            reply_markup=reply_markup,
+        )
+        self._set_previous_message_id(context, new_message)
+        return cast(Message, new_message)
