1bf03d70d4ad435a04096bcbeafe208b34c46649
Hanming Zhu <hanming@huawen.com>
2022-08-14 22:55:32 +0800

    feat: update utils exports

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -6,9 +6,12 @@ from .exceptions import (
     InvalidUserDataException,
     ResourceNotFoundException,
 )
+from .platforms import format_platform_name
 from .schemata import UUID_REGEX, UUID_RULE, UUIDS_RULE, validate_input
 from .time import (
+    MONTH_ALL_SUMMARY_STRFTIME_FORMAT,
     TIMEZONE,
+    WEEK_SUMMARY_STRFTIME_FORMAT,
     get_start_of_last_week,
     get_start_of_month,
     get_start_of_week,
